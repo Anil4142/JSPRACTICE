67795b7aa85c2385e10a8bb52d57c0d37853aa10
Anil4142 <anianilkumar884@gmail.com>
2022-11-29 22:48:06 +0530

    aruna

---
+++ ASSIGNMENTSJS/pos.js
@@ -0,0 +1,41 @@
+
+/*let a=33;
+if(a>0){
+    console.log("This num is positive")
+    
+}
+else if(a==0){
+    console.log("The num is equal to zero")
+
+}
+else {
+     console.log("The num is negative")
+} */
+
+
+
+
+/*let a=0;
+if(a>0){
+    console.log("The num is positive")
+}
+else if(a==0){
+    console.log("the num is equal to zero")
+
+}
+else{
+ console.log("The num is negative")
+}*/
+
+
+let a=-55
+if(a>0){
+    console.log("This num is positive")
+
+}
+else if(a==0){
+     console.log("The num is equal to zero")
+}
+else{
+    console.log("The num is negative")
+}
