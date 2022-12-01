39e2e05ae44965d962143cc6b8b5f87b949e101f
Anil4142 <anianilkumar884@gmail.com>
2022-12-01 15:54:10 +0530

    okie

---
+++ FUNCTIONS/age.js
@@ -0,0 +1,18 @@
+function age(year){
+                console.log (2022-year)
+                function age1(month){
+                    console.log(12-month)
+                }
+                age1(08)
+                {
+                    function age2(date){
+                        console.log(30-date)
+                    }
+                    age2(26)
+                }
+}
+age(2000)
+
+
+                                                            // it is correct
+                                                            //But perfectly not matched to my idea
