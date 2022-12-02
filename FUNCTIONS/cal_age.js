46425622eaa7d4d2fce8e438e5fc45b5ac61ff05
Anil4142 <anianilkumar884@gmail.com>
2022-12-02 17:54:06 +0530

    okie

---
+++ FUNCTIONS/cal_age.js
@@ -0,0 +1,15 @@
+/*function data(name,year){
+           let no_of_years=2022-year;
+           return "Employee name :"+  name  +    "          Age:" + no_of_years
+}
+let r= data("anil",2000)
+console.log(r)*/
+
+
+function  age(name,year){
+    let present_age = 2022-year
+    return "Employee Name:"+ name + "  Age:"+ present_age
+}
+let a= age("narasimha",1999)
+console.log(a)
+
