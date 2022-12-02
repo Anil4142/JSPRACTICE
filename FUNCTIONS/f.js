e6456a87ac4c51970cfdcb12e5ea498894f33f83
Anil4142 <anianilkumar884@gmail.com>
2022-12-02 17:25:48 +0530

    okie

---
+++ FUNCTIONS/f.js
@@ -0,0 +1,9 @@
+function add(){
+    console.log("Baby boy")
+    console.log("Baby girl")
+    console.log("Cute kid")
+
+}
+add(20,52)         //if parameter are not there only inner code will run but u need to use invoke .if we don't use invoke ,output we don't get.
+add(20,52,88)       // we can use N Number of same invoke
+add(20,52,52,52)        
---
+++ FUNCTIONS/get.js
@@ -0,0 +1,21 @@
+/*function get_data(a,b){
+   console.log("one")
+    console.log("two")
+    console.log(a+b)
+    return("three")
+    
+}
+get_data("narasimha",10)
+let a=get_data()
+console.log(a)*/
+
+
+function add(a,b){
+    console.log(a+b)
+    console.log(a*b)
+    return (a/b)
+}
+
+//add(20,5)
+let r=add(20,5)
+console.log(r)
